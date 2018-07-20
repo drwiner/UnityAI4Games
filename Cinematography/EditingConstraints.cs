68596a1982b71d2214d6b3d986d70b9bf440bf77
drwiner <drwiner131@gmail.com>
2018-07-20 18:57:11 -0400

    updates

---
+++ Cinematography/EditingConstraints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoltFreezer.Camera.CameraEnums
+{
+    public enum EditingConstraints
+    {
+        NoJumpCuts,
+        LineOfSight,
+        d180_Rule,
+        ShotLength_25,
+        ShotLength_3,
+        ShotLength_35,
+        ShotLength_4
+    }
+}
